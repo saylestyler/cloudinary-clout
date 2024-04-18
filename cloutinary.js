9a4dceadc07359cf40bf638046080761843c755a
Tyler Sayles <saylestyler@gmail.com>
2024-04-18 01:54:16 -0400

    rename env vars

--- cloutinary.js
+++ cloutinary.js
@@ -12,9 +12,9 @@ const chokidar = require('chokidar')
 dotenv.config()
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
 /**
